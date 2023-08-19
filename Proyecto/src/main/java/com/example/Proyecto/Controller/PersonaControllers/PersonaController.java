e20be22d31ae523b20e76940705a2e9bf6c90730
JuanLaur <joselaura472@gmail.com>
2023-08-18 20:18:41 -0400

    JJ

--- Proyecto/src/main/java/com/example/Proyecto/Controller/PersonaControllers/PersonaController.java
+++ Proyecto/src/main/java/com/example/Proyecto/Controller/PersonaControllers/PersonaController.java
@@ -1,5 +1,40 @@
 package com.example.Proyecto.Controller.PersonaControllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.Proyecto.Models.Entity.Persona;
+import com.example.Proyecto.Models.IService.IPersonaService;
+import com.example.Proyecto.Models.Otros.Encryptar;
+
+@Controller
+@RequestMapping("/adm")
 public class PersonaController {
+    
+    @Autowired
+	private IPersonaService personaService;
+    
+    @RequestMapping(value = "PersonaR", method = RequestMethod.GET)
+    public String PersonaR(@Validated Persona persona, Model model) throws Exception {
+
+        List<Persona> personas = personaService.findAll();
+        List<String> encryptedIds = new ArrayList<>();
+        for (Persona persona2 : personas) {
+            String id_encryptado = Encryptar.encrypt(Long.toString(persona2.getId_persona()));
+            encryptedIds.add(id_encryptado);
+        }
+        model.addAttribute("personas", personas);
+        model.addAttribute("id_encryptado", encryptedIds);
+        
+        return "persona/persona-adm";
+    }
+
     
 }
--- Proyecto/src/main/java/com/example/Proyecto/Controller/PersonaControllers/PersonaRestController.java
+++ Proyecto/src/main/java/com/example/Proyecto/Controller/PersonaControllers/PersonaRestController.java
@@ -1,5 +1,35 @@
 package com.example.Proyecto.Controller.PersonaControllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.Proyecto.Models.Entity.Persona;
+import com.example.Proyecto.Models.IService.IPersonaService;
+import com.example.Proyecto.Models.IService.IUsuarioService;
+
+@Controller
 public class PersonaRestController {
+
+    @Autowired
+	private IPersonaService personaService;
+
+    @Autowired
+	private IUsuarioService usuarioService;
     
+    @RequestMapping(value = "/PersonaF", method = RequestMethod.POST) // Enviar datos de Registro a Lista
+	public String PersonaF(@Validated Persona persona) { // validar los datos capturados (1)
+
+		persona.setEstado_persona("A");
+		personaService.save(persona);
+
+        usuarioService.insertar_adm(persona.getEmail_persona(), persona.getCi_persona(),
+                    Math.toIntExact(persona.getId_persona()));
+
+        
+
+		return "redirect:/adm/PersonaR";
+	}
 }
