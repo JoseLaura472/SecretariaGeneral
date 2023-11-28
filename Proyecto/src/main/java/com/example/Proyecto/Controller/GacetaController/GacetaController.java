4665adccd4a70c23a225703786f14854c3e2f0d2
abrahan24 <asaraviacoca@gmail.com>
2023-11-28 18:17:44 -0400

    UPDATE_ABRAHAN 28-11-2023

--- Proyecto/src/main/java/com/example/Proyecto/Controller/GacetaController/GacetaController.java
+++ Proyecto/src/main/java/com/example/Proyecto/Controller/GacetaController/GacetaController.java
@@ -1,10 +1,28 @@
 package com.example.Proyecto.Controller.GacetaController;
 
+import java.time.ZoneId;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.Proyecto.Models.Entity.Resolucion;
+import com.example.Proyecto.Models.IService.IConsejoService;
+import com.example.Proyecto.Models.IService.IResolucionService;
 
 @Controller
 public class GacetaController {
+
+    @Autowired
+    private IResolucionService resolucionService;
+
+    @Autowired
+    private IConsejoService consejoService;
     
     @GetMapping(value = "/gaceta")
     public String vista_gaceta_inicio(){
@@ -12,7 +30,21 @@ public class GacetaController {
         return "gaceta/gaceta";
     }
     @GetMapping(value = "/gaceta_res")
-    public String vista_gaceta_resoluciones(){
+    public String vista_gaceta_resoluciones(Model model){
+
+        List<Resolucion> resoluciones;
+
+        resoluciones = resolucionService.findAll();
+
+        Set<Integer> years = resoluciones.stream()
+                .map(resolucion -> resolucion.getFecha_resolucion().toInstant().atZone(ZoneId.systemDefault())
+                        .toLocalDate().getYear())
+                .collect(Collectors.toSet());
+
+        model.addAttribute("years", years.stream()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList()));
+        model.addAttribute("consejos", consejoService.findAll());
 
         return "gaceta/gaceta_res";
     }
